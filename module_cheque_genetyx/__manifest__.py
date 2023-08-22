6dc20f2fe02590defb552cf193e65d1187e6a2c1
gcc <gabriel.caceres@sati.com.py>
2023-08-22 07:51:13 -0400

    se agrega licence en manifest

--- module_cheque_genetyx/__manifest__.py
+++ module_cheque_genetyx/__manifest__.py
@@ -12,6 +12,7 @@
 
     'author': "Sati",
     'website': "http://sati.com.py",
+    'license': 'LGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
